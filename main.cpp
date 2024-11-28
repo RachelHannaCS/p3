7a14088dcab7185f9c2cf81bbecc2cc1e5235f68
RachelHannaCS <159095906+RachelHannaCS@users.noreply.github.com>
2024-11-28 12:29:37 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ int array = 40;
 
 int main () {
     // member variables Name, Test1, Test2, Test3, Final, Course
-    string name, course;
+    string name, course, header;
     int Test1, Test2, Test3, Final, choice;
     ifstream fileIn;
+    ofstream fileOut;
 
     // try/cast for input file 
     try {
@@ -21,6 +22,42 @@ int main () {
     }
         // if not found, exit program
     
+    // header
+	getline(fileIn, header);
+	fileOut << "Student               T1     T2     T3  Final   Drop  Total  SID     Grade" << endl;
+	// record detail
+    
+    // menu 
+
+	// switch statement
+	switch(choice) {
+	    int choice;
+	case 1:
+		// enter name of output file
+		break;
+	case 2:
+		// sort by name (ascending A->Z)
+		break;
+	case 3:
+		// sort by student ID (ascending)
+		break;
+	case 4:
+		// output to screen
+		break;
+	case 5:
+		// output to file
+		break;
+	case 6:
+		// quit
+		break;
+	default:
+		cout << "Enter menu choice: ";
+	    cin >> choice;
+		break;
+	}
+	while (choice < 6);
+	
     return 0;
 }
+
 
