8516f00799c9aee2b0b352c4a140f2ef09d2a8b1
RachelHannaCS <159095906+RachelHannaCS@users.noreply.github.com>
2024-11-29 21:50:52 -0800

    Update exam.h

--- exam.h
+++ exam.h
@@ -10,19 +10,15 @@ class studType {
   // member variables Name, Test1, Test2, Test3, Final, Course (public)
   public:
   string Name, Course;
-  int Test1, Test2, Test3, Final;
- // getGrade(), outputFile() public member, outputScreen() friend
- double getGrade();
- int dropTest(); 
- void outputScreen(studType students[], int numStud);
- void outputFile(studType students[], int numStud);
- 
+  int Test1, Test2, Test3, Final, SID;
  // create default constructor w base class 
 
  studType() : studentID("Null"), Name("Null"), Course("Null"), Test1(0), Test2(0), Test3(0), Final(0) {}
  // create parameterized constructor w base class 
  studType (string Name, int Test1, int Test2, int Test3, int Final);
 
+ int dropTest(int test1, int test2, int test3);
+ int dropTest(int test1, int test2, int test3, int final);
  // create void printInfo
  void printInfo();
  
@@ -41,5 +37,5 @@ string getStudentID() {
 };
 // sub classes 
 class cs115: public studType {};
-class cs120 :public studType{};
+class cs120: public studType{};
 #endif
